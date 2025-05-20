fb2dc50acebfacd63190c6e60b40dcdbc1c1abd5
SSTZCH <stephenstedja@gmail.com>
2025-05-20 19:45:48 +0700

    all done

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_character_count, sort_character
 
 def get_book_text(path_to_file):
@@ -5,18 +6,23 @@ def get_book_text(path_to_file):
         return f.read()
 
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
-    num_words = get_num_words(book_text)
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    counts = get_character_count(book_text)
-    listing = sort_character(counts)
-    for item in listing:
-        if item["char"].isalpha():
-            print(f"{item['char']}: {item['num']}")
-    print("============= END ===============")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        path_to_file = sys.argv[1]
+        book_text = get_book_text(path_to_file)
+        num_words = get_num_words(book_text)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {path_to_file}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        counts = get_character_count(book_text)
+        listing = sort_character(counts)
+        for item in listing:
+            if item["char"].isalpha():
+                print(f"{item['char']}: {item['num']}")
+        print("============= END ===============")
 
 main()
